c4aa1eec215eec69c96008d611f2f75437efa128
Yuri Gravata <ygravata@gmail.com>
2021-05-15 01:52:03 -0300

    finishing class preparation

---
+++ app/javascript/controllers/counter_controller.js
@@ -0,0 +1,23 @@
+import { Controller } from "stimulus";
+
+export default class extends Controller {
+  static targets = ['count'];
+
+  // connect() {
+  //   console.log(this.countTarget);
+  // }
+
+  connect() {
+    setInterval(this.refresh, 5000);
+  }
+
+
+  refresh = () => {
+    fetch('/restaurants', { headers: { accept: "application/json" } })
+      .then(response => response.json())
+      .then((data) => {
+        this.countTarget.innerText = data.restaurants.length;
+      });
+  }
+
+}
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :restaurants, only: [ :index, :show ] do
+  root to: 'pages#home'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :restaurants, only: [ :index, :show, :destroy ] do
     resources :reviews, only: :create
   end
 end
